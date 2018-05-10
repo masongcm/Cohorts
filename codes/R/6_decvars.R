61b8c954e8d53933b0cbf7c38f131b05bef43ff0
Giacomo Mason <mason.gcm@gmail.com>
2018-05-10 12:09:14 +0100

    added interactions

--- codes/R/6_decvars.R
+++ codes/R/6_decvars.R
@@ -9,14 +9,21 @@ decvarslist$mch <- c("mothageb", "teenm", "mheight", "singlem", "mempl", "ethn")
 decvarslist$prg <- c("parity", "firstb", "nprevst", "smkpr")
 decvarslist$bth <- c("caesbirth", "gestaw", "preterm", "bwt", "lowbwt")
 
+# interaction between mother characteristics and initial conditions
+mchxbth <-  list(apply(
+  expand.grid(decvarslist$mch, decvarslist$bth),
+  MARGIN=1, FUN = function(x) paste0(x,collapse = "*")))
+decvarslist_int <- c(decvarslist, mchxbth)
+
 # names of the groups
-decvarsgroups <- c("Social Class", "Maternal Education", "Maternal Charact.", "Pregnancy", "Birth")
+decvarsgroups <- c("Social Class", "Mat. Educ.", "Mat. Charact.", "Pregnancy", "Birth", "Birth * Mat. Char.")
 
 # make into vector
 decvars <- unname(unlist(decvarslist))
+decvars_int <- unname(unlist(decvarslist_int))
 
 # decomposition sequence
-decseq <- lapply(decvarslist, FUN = function(x) paste0(x, collapse="+"))
+decseq <- lapply(decvarslist_int, FUN = function(x) paste0(x, collapse="+"))
 
 # final data for analysis and decomposition
 finaldata <- scores2plot[complete.cases(scores2plot[,decvars]),]

c03ce494017d07177ad24b5a6e927b74701a6c8e
Giacomo Mason <mason.gcm@gmail.com>
2018-05-13 15:58:04 +0100

    separate bind chunk

--- codes/R/4_scores.R
+++ codes/R/4_scores.R
@@ -4,4 +4,8 @@
 ## ---- FA_SCORES_MAIN
 # score model selected as final
 scores.final <- predict(finalmod, newdata = items[[1]])
+
+############################################################################################
+## ---- FA_SCORES_BIND
+# bind scores with data
 scores2plot <- cbind(items[[1]], do.call(rbind, scores.final))

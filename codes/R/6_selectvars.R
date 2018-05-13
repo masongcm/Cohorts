d8ff02dc169058d830ea975c264b0a612fef6e62
Giacomo Mason <mason.gcm@gmail.com>
2018-05-13 15:56:41 +0100

    export to stata

--- codes/R/6_selectvars.R
+++ codes/R/6_selectvars.R
@@ -4,10 +4,10 @@
 # select variables for decomposition, in groups
 decvarslist <- list()
 decvarslist$ses <- c("scl10b")
-decvarslist$med <- c("mysch5b")
-decvarslist$mch <- c("mothageb", "teenm", "mheight", "singlem", "mempl", "ethn")
-decvarslist$prg <- c("parity", "firstb", "nprevst", "smkpr")
-decvarslist$bth <- c("caesbirth", "gestaw", "preterm", "bwt", "lowbwt")
+decvarslist$med <- c("mpsla5")
+decvarslist$mem <- c("mempl5")
+decvarslist$mch <- c("mothageb", "teenm", "mheight", "singlem", "ethn")
+decvarslist$prg <- c("parity", "firstb", "nprevst", "smkpr", "caesbirth", "gestaw", "preterm", "bwt", "lowbwt")
 
 # interaction between mother characteristics and initial conditions
 mchxbth <-  list(apply(
@@ -16,15 +16,17 @@ mchxbth <-  list(apply(
 decvarslist_int <- c(decvarslist, mchxbth)
 
 # names of the groups
-decvarsgroups <- c("Social Class", "Mat. Educ.", "Mat. Charact.", "Pregnancy", "Birth", "Birth * Mat. Char.")
+decvarsgroups <- c("Social Class", "Mat. Education", "Mat. Employment", "Mat. Charact.", "Pregnancy/Birth")
 
 # make into vector
 decvars <- unname(unlist(decvarslist))
 decvars_int <- unname(unlist(decvarslist_int))
 
-# decomposition sequence
-decseq <- lapply(decvarslist_int, FUN = function(x) paste0(x, collapse="+"))
+# decomposition sequence (without interactions)
+decseq <- lapply(decvarslist, FUN = function(x) paste0(x, collapse="+"))
 
 # final data for analysis and decomposition
 finaldata <- scores2plot[complete.cases(scores2plot[,decvars]),]
 
+# export to stata for Gelbach
+export(finaldata, paste0(dir_data, "finaldata.dta"))

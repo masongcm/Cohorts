74efd6260b0da141d538d9903c9a98e3db9d4f31
Giacomo Mason <mason.gcm@gmail.com>
2018-05-16 08:56:46 +0100

    remove incineq

--- codes/R/5_graphs.R
+++ codes/R/5_graphs.R
@@ -114,35 +114,6 @@ legend_b <- get_legend(denslist[[1]] + theme(legend.position="bottom"))
 p <- plot_grid( pcol, legend_b, ncol = 1, rel_heights = c(1, .1))
 p
 
-############################################################################################
-## ---- INCINEQ
-# MEAN/CI plot of income by income quintile
-
-# aggregate mean income
-semean <- function(x) sqrt(var(x,na.rm=TRUE)/length(na.omit(x)))
-meaninc <- data.frame(
-  aggregate(scores2plot$faminc10_real, 
-            by = list(scores2plot$cohort, scores2plot$incq), 
-            FUN=function(x) c(m = mean(x, na.rm=T), se = semean(x) )))
-meaninc <- cbind(meaninc[1:2], as.matrix(meaninc$x))
-colnames(meaninc) <- c("cohort", "quant", "mean", "semean")
-
-# restandardise
-q1.bcs <- meaninc[meaninc$cohort=="BCS" & meaninc$quant=="1", "mean"]
-q1.mcs <- meaninc[meaninc$cohort=="MCS" & meaninc$quant=="1", "mean"]
-meaninc[meaninc$cohort=="BCS", "mean"] <- (meaninc[meaninc$cohort=="BCS", "mean"] - q1.bcs)/q1.bcs
-meaninc[meaninc$cohort=="MCS", "mean"] <- (meaninc[meaninc$cohort=="MCS", "mean"] - q1.mcs)/q1.mcs
-meaninc$ciu <- meaninc$mean + 1.96*meaninc$semean
-meaninc$cil <- meaninc$mean - 1.96*meaninc$semean
-
-ineq.inc <- ggplot(data=meaninc, aes(x=as.factor(quant), y=mean, colour=cohort)) +
-  geom_point(size=3) + 
-  scale_y_continuous(name = "Mean Family Income", breaks = seq(0,11,2)) +
-  xlab("Family Income Quintile at 10") +
-  theme(legend.justification=c(0,0), legend.position=c(0,.8), legend.title = element_blank())
-
-ineq.inc
-
 
 ############################################################################################
 ## ---- FACINEQ

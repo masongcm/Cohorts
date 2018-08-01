45341036da47a08b0bea746a376616240445ef1d
Giacomo Mason <mason.gcm@gmail.com>
2018-08-01 12:14:38 +0100

    add MT anchored model

--- codes/R/3_inv_main.R
+++ codes/R/3_inv_main.R
@@ -13,6 +13,7 @@ t.th.4 <- glue::collapse(readLines(paste0(dir_syntax, "t_th_4.lav")), sep=" \n  
 
 # WE THRESHOLD+LOADINGS invariance, theta par (Condition 19)
 tl.th.4 <- glue::collapse(readLines(paste0(dir_syntax, "tl_th_4.lav")), sep=" \n  ")
+tl.mt.4 <- glue::collapse(readLines(paste0(dir_syntax, "tl_mt_4.lav")), sep=" \n  ")
 
 # WE THRESHOLD+LOADINGS+INTERCEPT invariance, theta par (Condition 27)
 tli.th.4 <- glue::collapse(readLines(paste0(dir_syntax, "tli_th_4.lav")), sep=" \n  ")
@@ -22,20 +23,22 @@ tlip.th.4 <- glue::collapse(readLines(paste0(dir_syntax, "tlip_th_4.lav")), sep=
 
 # allocate empty lists
 fa.c <- list()
-fa.t <- list()
 fa.tl <- list()
+fa.tlmt <- list()
 fa.tli <- list()
 fa.tlip <- list()
 
 # MODEL 1: separate gender groups, not age adjusted
 fa.c[[1]]   <- cfa(c.th.4, data = items[[1]], group = "cohortsex", estimator="wlsmv", parameterization="theta")
 fa.tl[[1]]  <- cfa(tl.th.4, data = items[[1]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
+fa.tlmt[[1]]  <- cfa(tl.mt.4, data = items[[1]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 fa.tli[[1]] <- cfa(tli.th.4, data = items[[1]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 fa.tlip[[1]] <- cfa(tlip.th.4, data = items[[1]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 
 # MODEL 2: separate gender groups, overlapping ages, no age adjustment
 fa.c[[2]]   <- cfa(c.th.4, data = items[[2]], group = "cohortsex", estimator="wlsmv", parameterization="theta")
 fa.tl[[2]]  <- cfa(tl.th.4, data = items[[2]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
+fa.tlmt[[2]]  <- cfa(tl.mt.4, data = items[[2]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 fa.tli[[2]] <- cfa(tli.th.4, data = items[[2]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 fa.tlip[[2]] <- cfa(tlip.th.4, data = items[[2]], group = "cohortsex", estimator="wlsmv", parameterization="theta", group.equal='loadings')
 

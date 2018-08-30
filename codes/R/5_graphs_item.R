ed3b32ede2c0679ce8fe57913668e57d7c813a43
Giacomo Mason <mason.gcm@gmail.com>
2018-08-30 16:51:36 +0100

    add SEs to item plots

--- codes/R/5_graphs_item.R
+++ codes/R/5_graphs_item.R
@@ -16,7 +16,10 @@ s2pb <- s2pb %>%
   mutate(fscl5wb = fct_rev(fscl5wb),
          smkpr = fct_rev(smkpr))
 
+# function to get standard errors
+stderr <- function(x) sqrt(var(x,na.rm=TRUE)/length(na.omit(x)))
 
+###########################################################
 # function to return plot of items
 itemineq <- function(data, categvar,
                      ylab = "Prevalence Ratio",
@@ -26,19 +29,32 @@ itemineq <- function(data, categvar,
   categ_enq <- enquo(categvar) # tidy eval
 
   # compute within cohort differences in item prevalence by mother education
+  
   plotdata <- data %>%
     group_by(cohort, sex, !! categ_enq) %>%
     filter(!is.na(!! categ_enq)) %>% # nonmissing education
-    summarise_at(vars(starts_with('Xb')), funs(mean(.))) %>% # prevalence by item 
-    gather(key = item, value = prevalence, paste0("Xb", seq(1,11))) %>% # reshape to items in row
+    summarise_at(vars(starts_with('Xb')), funs(num=sum(!is.na(.)), # observations (n) 
+                                               succ=sum(.==1), # occurrences/successes (x)
+                                               mean = mean(.), # mean
+                                               se=stderr(.) # se of mean
+    )) %>%  
+    gather(key = item, value = prevalence, starts_with("Xb")) %>% # reshape to items in row
+    separate(item,c("item", "stat"), sep="_") %>% # separate item from statistic
+    spread(stat, prevalence) %>%
+    arrange(cohort, sex, item, !! categ_enq) %>%
     group_by(cohort, sex, item) %>% 
-    mutate(diff = lag(prevalence, default = NA) - prevalence) %>% # get differences between levels of mpsla5
-    mutate(prevratio = lag(prevalence, default = NA) / prevalence) %>% # get differences between levels of mpsla5
+    mutate(diff = lag(mean, default = NA) - mean, # differences in prevalence
+           diffse = sqrt( (lag(se, default = NA)^2)/lag(num, default = NA) + se^2/num ), # SE of difference
+           diffub = diff+1.96*diffse, difflb = diff-1.96*diffse, # difference CI
+           pratio = lag(mean, default = NA) / mean, # ratio of prevalence
+           lpratiose = sqrt( 1/(lag(succ, default = NA)) - 1/(lag(num, default = NA)) + 1/succ - 1/num ), # SE of ratio
+           pratioub = pratio*exp(1.96*lpratiose), pratiolb = pratio*exp(-1.96*lpratiose) # ratio CI
+    ) %>% 
     filter(!is.na(diff)) %>% # only non-null differences
     mutate(itemn = readr::parse_number(item)) %>%
-    select(cohort, sex, item, itemn, prevratio, diff) %>%
+    select(cohort, sex, item, itemn, pratio, lpratiose, pratioub, pratiolb, diff, diffse, diffub, difflb) %>%
     mutate(fac = ifelse(itemn > 6, "INT", "EXT"),
-           diff = diff*100)
+           diff = diff*100, diffub = diffub*100, difflb = difflb*100)
   
   # item labels
   plotdata$iteml <- factor(plotdata$itemn)
@@ -56,8 +72,8 @@ itemineq <- function(data, categvar,
   
   # get maximum ratio for graph limit
   if (is.null(ylim)) {
-    maxy <- max(plotdata$prevratio, na.rm = TRUE)+0.05
-    miny <- min(plotdata$prevratio, na.rm = TRUE)-0.05
+    maxy <- max(plotdata$pratio, na.rm = TRUE)+0.05
+    miny <- min(plotdata$pratiolb, na.rm = TRUE)-0.05
     ylim <- c(maxy,miny)
   }
 
@@ -68,15 +84,17 @@ itemineq <- function(data, categvar,
       scale_y_continuous(name = ylab) +
       coord_cartesian(ylim = ylim) +
       geom_hline(yintercept = 1, linetype="dashed") +
-      geom_bar(stat = "identity", position = "dodge", width = 0.5, alpha = .75) +
+      geom_bar(stat = "identity", position = "dodge", width = 0.5, alpha = .5) + # bar
+      geom_errorbar(aes(ymin=pratiolb, ymax=pratioub), width = 0.25, # CI
+                    position=position_dodge(.5)) +
       labs(list(fill=""))
     return(x)
   }
   
-  prev.ext.m <- ggplot(subset(plotdata, sex == "M" & fac == "EXT"), aes(iteml, prevratio, fill = cohort)) + ggtitle("Males Externalising")
-  prev.int.m <- ggplot(subset(plotdata, sex == "M" & fac == "INT"), aes(iteml, prevratio, fill = cohort)) + ggtitle("Males Internalising")
-  prev.ext.f <- ggplot(subset(plotdata, sex == "F" & fac == "EXT"), aes(iteml, prevratio, fill = cohort)) + ggtitle("Females Externalising")
-  prev.int.f <- ggplot(subset(plotdata, sex == "F" & fac == "INT"), aes(iteml, prevratio, fill = cohort)) + ggtitle("Females Internalising")
+  prev.ext.m <- ggplot(subset(plotdata, sex == "M" & fac == "EXT"), aes(iteml, pratio, fill = cohort)) + ggtitle("Males Externalising")
+  prev.int.m <- ggplot(subset(plotdata, sex == "M" & fac == "INT"), aes(iteml, pratio, fill = cohort)) + ggtitle("Males Internalising")
+  prev.ext.f <- ggplot(subset(plotdata, sex == "F" & fac == "EXT"), aes(iteml, pratio, fill = cohort)) + ggtitle("Females Externalising")
+  prev.int.f <- ggplot(subset(plotdata, sex == "F" & fac == "INT"), aes(iteml, pratio, fill = cohort)) + ggtitle("Females Internalising")
   prevlist <- list(prev.ext.m, prev.int.m, prev.ext.f, prev.int.f)
   prevlist <- lapply(prevlist, function(x) addopts_prev(x)) # apply options to all graphs
   pcol <- plot_grid( prevlist[[1]], prevlist[[3]], prevlist[[2]], prevlist[[4]],
